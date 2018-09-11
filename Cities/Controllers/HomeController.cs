21f61b4e861bb666a9e558d153d18bd8e7770007
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-11 10:41:17 +0400

    Creating the project

---
+++ Cities/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cities.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index() => View(repository.Cities);
+        public ViewResult Create() => View();
+        [HttpPost]
+        public IActionResult Create(City city)
+        {
+            repository.AddCity(city);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Cities/Startup.cs
+++ Cities/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cities.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,20 +16,17 @@ namespace Cities
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IRepository, MemoryRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
